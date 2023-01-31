91ea773cae3bf33defc963b04159454f84fac253
Joy Karmakar <96533682+joy095@users.noreply.github.com>
2023-01-31 12:07:18 +0530

    project push

---
+++ server/routes/dalleRoutes.js
@@ -0,0 +1,39 @@
+import express, { response } from "express";
+import * as dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const router = express.Router();
+
+const configration = new Configuration({
+  apiKey: process.env.OPEN_API_KEY,
+});
+
+const openapi = new OpenAIApi(configration);
+
+router.route("/").get((req, res) => {
+  res.send("Hello from DALLE-E!");
+});
+
+router.route("/").post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const aiResponse = await openapi.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+
+    const image = aiResponse.data.data[0].b64_json;
+
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error?.response.data.error.message);
+  }
+});
+
+export default router;
